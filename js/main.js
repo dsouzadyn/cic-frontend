786cb3bc67989676287547e2dc85cf86298707d1
Dylan Dsouza <dsouzadyn@gmail.com>
2019-02-28 23:33:08 +0530

    added tags

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ function uploadAndCheck() {
     var text1 = document.getElementById('text1').value;
     var text2 = document.getElementById('text2').value;
 
-    const data = { 'Rawtext': text1 };
+    const data = { 'Input1': text1, 'Input2': text2 };
 
     const options = {
         method: 'POST',
@@ -20,22 +20,38 @@ function uploadAndCheck() {
     //     </div>
     // </div>
 
-    var parent = document.getElementById('tags');
-    parent.innerHTML = ""
+    var parent1 = document.getElementById('tags1');
+    var parent2 = document.getElementById('tags2');
+    var cosineValue = document.getElementById('cosine_value');
+    
+    parent1.innerHTML = "";
+    parent2.innerHTML = "";
 
     if (text1 !== "" && text2 !== "") {
         axios(options)
         .then((res) => {
             var data = res.data;
-            for (var word in data.term_frequencies) {
+            console.log(data);
+            cosineValue.innerText = (data.cosine_value*100).toString() + " %";
+            for (var word in data.term_frequencies_doc1) {
                 var template = 
                     '<div class="control">' +
                     '<div class="tags has-addons">' +
                     '<span class="tag is-dark">' + word + '</span>' +
-                    '<span class="tag is-info">' + data.term_frequencies[word] + '</span>' +
+                    '<span class="tag is-info">' + data.term_frequencies_doc1[word] + '</span>' +
                     '</div>' +
                     '</div>';
-                parent.innerHTML += template;
+                parent1.innerHTML += template;
+            }
+            for (var word in data.term_frequencies_doc2) {
+                var template = 
+                    '<div class="control">' +
+                    '<div class="tags has-addons">' +
+                    '<span class="tag is-dark">' + word + '</span>' +
+                    '<span class="tag is-success">' + data.term_frequencies_doc2[word] + '</span>' +
+                    '</div>' +
+                    '</div>';
+                parent2.innerHTML += template;
             }
             
             document.querySelector('#metrics').scrollIntoView({
