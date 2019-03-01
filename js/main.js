36ea4a1b51fa6d63415256006a51280a1c8168fa
Dylan Dsouza <dsouzadyn@gmail.com>
2019-03-01 12:46:03 +0530

    updated ui for indicating max frequency

--- js/main.js
+++ js/main.js
@@ -24,6 +24,9 @@ function uploadAndCheck() {
     var parent2 = document.getElementById('tags2');
     var cosineValue = document.getElementById('cosine_value');
     
+    var max1 = 0;
+    var max2 = 0;
+
     parent1.innerHTML = "";
     parent2.innerHTML = "";
 
@@ -31,14 +34,17 @@ function uploadAndCheck() {
         axios(options)
         .then((res) => {
             var data = res.data;
-            console.log(data);
             cosineValue.innerText = (data.cosine_value*100).toString() + " %";
+            max1 = Math.max.apply(null, Object.keys(data.term_frequencies_doc1).map(function (key) { return data.term_frequencies_doc1[key];}));
+            max2 = Math.max.apply(null, Object.keys(data.term_frequencies_doc2).map(function (key) { return data.term_frequencies_doc2[key];}));
             for (var word in data.term_frequencies_doc1) {
+                console.log(data.term_frequencies_doc1[word] === max1)
                 var template = 
                     '<div class="control">' +
                     '<div class="tags has-addons">' +
                     '<span class="tag is-dark">' + word + '</span>' +
-                    '<span class="tag is-info">' + data.term_frequencies_doc1[word] + '</span>' +
+                    '<span class="tag ' + ((data.term_frequencies_doc1[word] === max1) ? 'is-warning">' : 'is-info">');
+                    template += data.term_frequencies_doc1[word] + '</span>' +
                     '</div>' +
                     '</div>';
                 parent1.innerHTML += template;
@@ -48,7 +54,8 @@ function uploadAndCheck() {
                     '<div class="control">' +
                     '<div class="tags has-addons">' +
                     '<span class="tag is-dark">' + word + '</span>' +
-                    '<span class="tag is-success">' + data.term_frequencies_doc2[word] + '</span>' +
+                    '<span class="tag ' + ((data.term_frequencies_doc2[word] === max2) ? 'is-warning">' : 'is-info">');
+                    template += data.term_frequencies_doc2[word] + '</span>' +
                     '</div>' +
                     '</div>';
                 parent2.innerHTML += template;
